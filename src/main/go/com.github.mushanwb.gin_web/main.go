9addcb595011564623355ad8aa93292b7df2b338
wangbo <1216476406@qq.com>
2021-01-05 16:49:35 +0800

    由于 utils 包中的 config 文件也有 init ，并且依赖 logger 文件中的 变量，会出现错误，因此需要将两个 init 放入不同的包中，并且在 main 中初始化

--- src/main/go/com.github.mushanwb.gin_web/main.go
+++ src/main/go/com.github.mushanwb.gin_web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"gin_web/src/main/go/com.github.mushanwb.gin_web/bootstrap"
 	_ "gin_web/src/main/go/com.github.mushanwb.gin_web/config"
+	_ "gin_web/src/main/go/com.github.mushanwb.gin_web/logger"
 )
 
 func main() {
