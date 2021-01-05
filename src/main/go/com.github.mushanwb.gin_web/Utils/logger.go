3cd72844166f8762d4f22f887807a4dff829588e
wangbo <1216476406@qq.com>
2021-01-05 16:14:00 +0800

    使用 sirupsen/logrus 依赖库，做日志系统

---
+++ src/main/go/com.github.mushanwb.gin_web/Utils/logger.go
@@ -0,0 +1,52 @@
+package Utils
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"time"
+)
+
+var Logger *logrus.Logger
+
+func init() {
+	now := time.Now()
+
+	logFilePath := ""
+
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+
+	if err := os.MkdirAll(logFilePath, 0777); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	logFileName := now.Format("2006-01-02") + ".log"
+
+	//日志文件
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	//写入文件
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	Logger = logrus.New()
+
+	Logger.SetLevel(logrus.DebugLevel)
+	Logger.SetReportCaller(true)
+	Logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+
+	//设置输出
+	Logger.Out = src
+
+}
